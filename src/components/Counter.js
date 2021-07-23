93d85d4c3ecbabf5b0feef35ec191d22633e6eca
Andrew Reynolds <andrew_reynolds@mac.com>
2021-07-22 17:16:16 -0700

    added payload to action

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -9,6 +9,10 @@ const Counter = () => {
   const incrementHandler = () => {
     dispatch({ type: "increment" });
   };
+
+  const increaseHandler = (number) => {
+    dispatch({ type: "increase", val: number });
+  };
   const decrementHandler = () => {
     dispatch({ type: "decrement" });
   };
@@ -19,6 +23,7 @@ const Counter = () => {
       <div className={classes.value}>{counter}</div>
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler.bind(null, 5)}>Increase by 5</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
