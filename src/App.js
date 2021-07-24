a559a6682d92bdfe93ce7eb20ded57b05b36ed99
Andrew Reynolds <andrew_reynolds@mac.com>
2021-07-24 10:57:11 -0700

    conditionally rendering auth component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Counter from "./components/Counter";
 import Header from "./components/Header";
 import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
+import { useSelector } from "react-redux";
 
 function App() {
+  const authState = useSelector((state) => state.auth.isAuthenticated);
   return (
     <>
       <Header />
-      <Auth />
+      {!authState ? <Auth /> : <UserProfile />}
       <Counter />
     </>
   );
