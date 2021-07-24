0603e4853f4f576104fece37b0650466d18a4435
Andrew Reynolds <andrew_reynolds@mac.com>
2021-07-24 10:47:19 -0700

    using multiple redux createSlices

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 // import { createStore } from "redux";
 import { createSlice, configureStore } from "@reduxjs/toolkit";
 
+const initialCounterState = { counter: 0, showCounter: true };
+
 const counterSlice = createSlice({
   name: "counter",
-  initialState: { counter: 0, showCounter: true },
+  initialState: initialCounterState,
   reducers: {
     increment(state) {
       state.counter++;
@@ -20,11 +22,29 @@ const counterSlice = createSlice({
   },
 });
 
+const initialAuthState = {
+  isAuthenticated: false,
+};
+
+const authSlice = createSlice({
+  name: "authentication",
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
 const appStore = configureStore({
-  reducer: counterSlice.reducer,
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
 });
 
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 export default appStore;
 
 // const counterReducer = (state = { counter: 0, showCounter: true }, action) => {
