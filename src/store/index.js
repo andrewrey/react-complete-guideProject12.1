630e0da2a4e667189d9830fdd9466d1670e2fa9f
Andrew Reynolds <andrew_reynolds@mac.com>
2021-07-23 23:17:41 -0700

    using configureStore

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
-import { createStore } from "redux";
-import { createSlice } from "@reduxjs/toolkit";
+// import { createStore } from "redux";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-createSlice({
+const counterSlice = createSlice({
   name: "counter",
   initialState: { counter: 0, showCounter: true },
   reducers: {
@@ -20,24 +20,26 @@ createSlice({
   },
 });
 
-const counterReducer = (state = { counter: 0, showCounter: true }, action) => {
-  switch (action.type) {
-    case "increment":
-      return { counter: state.counter + 1, showCounter: state.showCounter };
-    case "decrement":
-      return { counter: state.counter - 1, showCounter: state.showCounter };
-    case "increase":
-      return {
-        counter: state.counter + action.val,
-        showCounter: state.showCounter,
-      };
-    case "toggle":
-      return { counter: state.counter, showCounter: !state.showCounter };
-    default:
-      return state;
-  }
-};
-
-const appStore = createStore(counterReducer);
+const appStore = configureStore({
+  reducer: counterSlice.reducer,
+});
 
 export default appStore;
+
+// const counterReducer = (state = { counter: 0, showCounter: true }, action) => {
+//   switch (action.type) {
+//     case "increment":
+//       return { counter: state.counter + 1, showCounter: state.showCounter };
+//     case "decrement":
+//       return { counter: state.counter - 1, showCounter: state.showCounter };
+//     case "increase":
+//       return {
+//         counter: state.counter + action.val,
+//         showCounter: state.showCounter,
+//       };
+//     case "toggle":
+//       return { counter: state.counter, showCounter: !state.showCounter };
+//     default:
+//       return state;
+//   }
+// };
